fb4d4b64fe8b43891e25b77e2d14fc9758c3204f
giannifontanot <giannifontanot@gmail.com>
2021-07-26 17:32:27 -0500

    Added configuration and routes for static files

--- server.js
+++ server.js
@@ -11,9 +11,23 @@ const app = express();
 const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Routes to static HTML
+app.get('/',
+    (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+        console.log("landed  on root: ");
+    });
+app.get('/notes',
+    (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'))
+        console.log("landed on /notes: ");
+    });
+
+// ----------------------------------------------------- //
 
 // Routes to regular HTML
 app.get('/',

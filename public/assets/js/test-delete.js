da7ff317bea964937ce61cdaf01cdaad48511233
giannifontanot <giannifontanot@gmail.com>
2021-07-27 12:36:25 -0500

    Public route to js.

---
+++ public/assets/js/test-delete.js
@@ -0,0 +1,12 @@
+fetch('https://example.com/profile', {
+    method: 'POST', // or 'PUT'
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+})
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
